5aaf901e5d2aa228e9b952e2a803c7e445a6d2c1
Gerbiebor <kapiteinbadeend@gmail.com>
2020-08-06 11:45:16 +0200

    code

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(() => { 
+
+
+    $('#oprotten').click(function(){
+        $('audio#oprotten')[0].play()
+    })
+    $('#opkankeren').click(function(){
+        $('audio#opkankeren')[0].play()
+    })
+    $('#blocken').click(function(){
+        $('audio#blocken')[0].play()
+    })
+    $('#lach').click(function(){
+        $('audio#lach')[0].play()
+    })
+
+});
